d4911a6ba1619f29e586dd9a6ac96fcf739633c4
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 00:28:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,7 +6,7 @@ def square_array(array)
   new_numbers = []
 
   array.each do |element|
-    new_numbers[element] = element * element
+    new_numbers.push = element * element
   end
 
   return new_numbers
